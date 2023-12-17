69139d972e36e0d4db50772e536696cd931faeee
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-17 23:27:38 +0100

    Creat Session classe

---
+++ Classes/Session.php
@@ -0,0 +1,42 @@
+<?php
+
+class Session
+{
+    public function __construct()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+
+    public function hasGet($key)
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public function unset($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    public function destroy()
+    {
+        session_destroy();
+    }
+
+    public function start()
+    {
+        session_start();
+    }
+}
+?>
