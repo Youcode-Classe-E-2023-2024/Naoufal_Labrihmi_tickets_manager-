efa7c768634849105719c16ef0f41f1b9dcf438a
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-18 16:28:39 +0100

    update database sql

--- todo.sql
+++ todo.sql
@@ -1,15 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 18, 2023 at 10:47 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.1
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 18 déc. 2023 à 16:28
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,68 +18,76 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `todo`
+-- Base de données : `br`
 --
 
 -- --------------------------------------------------------
--- Table structure for table `todo`
---
-
-CREATE TABLE `todo` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `status` enum('todo','doing','done') NOT NULL DEFAULT 'todo',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `todo`
+-- Structure de la table `todo_developers`
 --
 
-INSERT INTO `todo` (`id`, `title`, `created_at`, `status`) VALUES
-(22, 'FrontEnd', '2023-01-18 23:33:48', 'todo'),
-(24, 'Dashbord', '2023-01-18 23:34:36', 'done'),
-(25, 'BackEnd', '2023-01-18 23:35:30', 'doing');
+CREATE TABLE `todo_developers` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `todo_id` int(11) UNSIGNED NOT NULL,
+  `developer_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Déchargement des données de la table `todo_developers`
+--
+
+INSERT INTO `todo_developers` (`id`, `todo_id`, `developer_id`) VALUES
+(4, 1, 4),
+(5, 1, 2),
+(6, 1, 8),
+(7, 25, 2),
+(8, 25, 3),
+(9, 26, 3),
+(10, 27, 1),
+(11, 28, 1),
+(12, 28, 2),
+(13, 28, 3),
+(14, 29, 2),
+(15, 29, 3),
+(18, 31, 2),
+(19, 31, 3),
+(34, 38, 1),
+(35, 38, 2),
+(36, 38, 3);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `todo`
+-- Index pour la table `todo_developers`
 --
-ALTER TABLE `todo`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+ALTER TABLE `todo_developers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_todo_id` (`todo_id`),
+  ADD KEY `idx_developer_id` (`developer_id`);
 
--- Add a new table for developers
-CREATE TABLE `developers` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
 
--- Insert some developers into the table
-INSERT INTO `developers` (`name`) VALUES
-('Naoufal'),
-('Walid'),
-('Mohsine'),
-('Ilyass'),
-('Hamza');
+--
+-- AUTO_INCREMENT pour la table `todo_developers`
+--
+ALTER TABLE `todo_developers`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
--- Add a new column to the `todo` table for developer assignment
-ALTER TABLE `todo`
-  ADD COLUMN `developer_id` int(11) UNSIGNED,
-  ADD CONSTRAINT `fk_todo_developer`
-  FOREIGN KEY (`developer_id`) REFERENCES `developers`(`id`)
-  ON DELETE SET NULL;
+--
+-- Contraintes pour les tables déchargées
+--
 
--- Example of assigning tasks to specific developers
-UPDATE `todo` SET `developer_id` = 1 WHERE `id` = 22;  -- Assign 'First 1' to Developer1
-UPDATE `todo` SET `developer_id` = 2 WHERE `id` = 24;  -- Assign 'Third' to Developer2
-UPDATE `todo` SET `developer_id` = 3 WHERE `id` = 25;  -- Assign 'Second' to Developer3;
-
--- End of the script
+--
+-- Contraintes pour la table `todo_developers`
+--
+ALTER TABLE `todo_developers`
+  ADD CONSTRAINT `fk_todo_dev_developer_id` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_todo_dev_todo_id` FOREIGN KEY (`todo_id`) REFERENCES `todo` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
