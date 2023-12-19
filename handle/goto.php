800f3f27cc402af8eb434524b6ae5d33a1cdeadd
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-19 16:17:27 +0100

    update goto file

--- handle/goto.php
+++ handle/goto.php
@@ -2,35 +2,49 @@
 
 require_once '../App.php';
 
-if($request->hasGet("id") && $request->hasGet("name")){
+if ($request->hasGet("id") && $request->hasGet("name") && $request->hasGet("referrer")) {
+    $id = $request->get("id");
+    $name = $request->get("name");
+    $referrer = $request->get("referrer");
 
-    $id=$request->get("id");
-    $name=$request->get("name");
-
-    $stm=$conn->prepare("select *  from todo where `id`=:id");
-    $stm->bindparam(":id",$id,PDO::PARAM_INT);
+    $stm = $conn->prepare("SELECT * FROM todo WHERE `id` = :id");
+    $stm->bindParam(":id", $id, PDO::PARAM_INT);
     $out = $stm->execute();
 
+    if ($out) {
+        switch ($name) {
+            case 'doing':
+            case 'done':
+                $stm = $conn->prepare("UPDATE todo SET `status` = :status WHERE id = :id");
+                $stm->bindParam(":id", $id, PDO::PARAM_INT);
+                $stm->bindParam(":status", $name, PDO::PARAM_STR);
+                $output = $stm->execute();
+                break;
+            case 'move-to-doing':
+                // Additional logic for moving to "Doing" from another section
+                $stmMoveToDoing = $conn->prepare("UPDATE todo SET `status` = 'doing' WHERE id = :id");
+                $stmMoveToDoing->bindParam(":id", $id, PDO::PARAM_INT);
+                $output = $stmMoveToDoing->execute();
+                break;
+            default:
+                $output = false;
+        }
 
-            if($out){
-                $stm=$conn->prepare("update todo set `status`= (:status) where id=(:id)");
-                $stm->bindparam(":id",$id,PDO::PARAM_INT);
-                $stm->bindparam(":status",$name,PDO::PARAM_STR);
-                $output=$stm->execute();
-                    if($output){
-                        $request->header("../index.php");
-                    }else{
-                        $request->header("../index.php"); 
-                    }
-       
-            }else{
-                $request->header("../index.php");
-            }
-
-
-
-
-
-}else{
-    $request->header("../index.php");
+        if ($output) {
+            // Redirect back to the referring page
+            $request->header("../" . $referrer);
+        } else {
+            $request->header("../" . $referrer);
+        }
+    } else {
+        $request->header("../" . $referrer);
+    }
+} else {
+    $request->header("../" . $referrer);
 }
+
+
+
+
+
+
