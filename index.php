0e5088f37c2cb083a68750f925c1f84508d8c3b7
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-20 20:19:27 +0100

    add img background

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
     </script>
 
     </head>
-    <body style="background-image: url(imgs/wp7969113-blurry-ultra-hd-wallpapers.jpg); background-size: cover; background-repeat: no-repeat; ">
+    <body style="background-image: url(imgs/wp9349630-blur-pc-4k-wallpapers.jpg); background-size: cover; background-repeat: no-repeat; ">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="index.php">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
