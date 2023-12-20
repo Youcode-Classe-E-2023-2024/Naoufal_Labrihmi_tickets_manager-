48c0269d2a7aca6ba049d4b583714bdb55f02169
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-20 16:52:58 +0100

    Update task_details.php

--- task_details.php
+++ task_details.php
@@ -94,11 +94,11 @@ $tags = $tagsQuery->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
 <!-- Comment Section -->
-<!-- Comment Section -->
 <div class="mt-4">
-<h4><strong>Comments</strong></h4>
-<!-- Add new comment form -->
-<form method="post" action="add_comment.php"> <!-- Create add_comment.php to handle form submission -->
+    <h4><strong>Comments</strong></h4>
+
+    <!-- Add new comment form -->
+    <form method="post" action="add_comment.php">
         <div class="mb-3">
             <label for="commentText" class="form-label">Add a Comment:</label>
             <textarea class="form-control" id="commentText" name="comment_text" rows="3" required></textarea>
@@ -107,36 +107,79 @@ $tags = $tagsQuery->fetchAll(PDO::FETCH_ASSOC);
         <button type="submit" class="btn btn-primary">Submit Comment</button>
     </form>
     <br>
-        <?php
-        // Fetch comments for the task
-        $commentsQuery = $conn->prepare("SELECT comments.*, developers.name AS commenter_name FROM comments INNER JOIN developers ON comments.posted_by = developers.id WHERE comments.task_id = :task_id");
-        $commentsQuery->bindParam(':task_id', $task_id, PDO::PARAM_INT);
-        $commentsQuery->execute();
-        $comments = $commentsQuery->fetchAll(PDO::FETCH_ASSOC);
 
-        // Display comments
-        if (!empty($comments)) {
-            foreach ($comments as $comment) {
-                ?>
-                <div class="card mb-2">
-                    <div class="card-body">
-                        <p class="card-text"><?= htmlspecialchars($comment['comment_text']) ?></p>
-                        <p class="card-subtitle text-muted">
-                            Posted by: <?= htmlspecialchars($comment['commenter_name']) ?> on <?= htmlspecialchars($comment['created_at']) ?>
-                        </p>
-                    </div>
+    <?php
+    // Fetch comments for the task
+    $commentsQuery = $conn->prepare("SELECT comments.*, developers.name AS commenter_name FROM comments INNER JOIN developers ON comments.posted_by = developers.id WHERE comments.task_id = :task_id");
+    $commentsQuery->bindParam(':task_id', $task_id, PDO::PARAM_INT);
+    $commentsQuery->execute();
+    $comments = $commentsQuery->fetchAll(PDO::FETCH_ASSOC);
+
+    // Display comments
+    if (!empty($comments)) {
+        foreach ($comments as $comment) {
+            ?>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <p class="card-text"><?= htmlspecialchars($comment['comment_text']) ?></p>
+                    <p class="card-subtitle text-muted">
+                        Posted by: <?= htmlspecialchars($comment['commenter_name']) ?> on <?= htmlspecialchars($comment['created_at']) ?>
+                    </p>
                 </div>
-                <?php
-            }
-        } else {
-            echo "<p>No comments yet.</p>";
-        }
-        ?>
-    </div>
-</div>
 
+                <!-- // Display replies for the comment -->
+<?php
+$repliesQuery = $conn->prepare("SELECT * FROM comment_replies WHERE comment_id = :comment_id");
+$repliesQuery->bindParam(':comment_id', $comment['id'], PDO::PARAM_INT);
+$repliesQuery->execute();
+$replies = $repliesQuery->fetchAll(PDO::FETCH_ASSOC);
+
+// Display replies
+if (!empty($replies)) {
+    foreach ($replies as $reply) {
+        // Fetch the developer name who added the reply
+        $replierQuery = $conn->prepare("SELECT name FROM developers WHERE id = :replied_by");
+        $replierQuery->bindParam(':replied_by', $reply['replied_by'], PDO::PARAM_INT);
+        $replierQuery->execute();
+        $replier = $replierQuery->fetch(PDO::FETCH_ASSOC);
+        ?>
+        <div class="card-footer">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text"><?= htmlspecialchars($reply['reply_text']) ?></p>
+                    <p class="card-subtitle text-muted">
+                        Replied by: <?= htmlspecialchars($replier['name']) ?> on <?= htmlspecialchars($reply['created_at']) ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+}
+?>
+
+
+                <!-- Add a reply form -->
+                <div class="card-footer">
+                    <form method="post" action="add_reply.php">
+                        <div class="mb-3">
+                            <label for="replyText" class="form-label">Reply to Comment:</label>
+                            <textarea class="form-control" id="replyText" name="reply_text" rows="2" required></textarea>
+                        </div>
+                        <input type="hidden" name="comment_id" value="<?= $comment['id'] ?>">
+                        <input type="hidden" name="task_id" value="<?= $task_id ?>">
+                        <button type="submit" class="btn btn-primary btn-sm">Submit Reply</button>
+                    </form>
+                </div>
+            </div>
+            <?php
+        }
+    } else {
+        echo "<p>No comments yet.</p>";
+    }
+    ?>
+</div>
     </div>
+    
 </body>
 </html>
-
-
